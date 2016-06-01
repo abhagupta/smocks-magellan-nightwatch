e2c86224937a92d60d95f043a05a0105a837ee01
Abha Gupta <agupta3@walmart.com>
2016-06-01 13:03:30 -0700

    did some changes for https investigation

--- example/mocks/run-mock-server.js
+++ example/mocks/run-mock-server.js
@@ -2,10 +2,17 @@
 
 // load our example endpoint
 require('./endpoints');
+var Fs = require('fs');
+
+var tls = {
+  key: Fs.readFileSync('/Users/agupt13/Documents/workspace/mocks/shifu_src/test/resources/key.pem'),
+  cert: Fs.readFileSync('/Users/agupt13/Documents/workspace/mocks/shifu_src/test/resources/cert.pem')
+};
 
 require('smocks/hapi').start({
   port: 8000,
-  host: 'localhost'
+  host: 'localhost',
+  tls
 }, function () {
   console.log('mock server started on port 8000');
 });
--- example/tests/nightwatch-init.js
+++ example/tests/nightwatch-init.js
@@ -1,9 +1,13 @@
 // this is used as the nightwatch global objct so we can have access
 // to the worker global setup and teardown.  this module is referenced
 // because it is defined as "globals_path" in the nightwatch.json file
-
+var fs = require('fs');
 module.exports = require('smocks-magellan-nightwatch').nightwatch({
 
   // point to our mock server hapi plugin
-  mockServer: require('../mocks/mock-server-hapi-plugin')
+  mockServer: require('../mocks/mock-server-hapi-plugin'),
+  tls: {
+    key: fs.readFileSync('/Users/agupt13/Documents/workspace/mocks/shifu_src/test/resources/key.pem'),
+    cert: fs.readFileSync('/Users/agupt13/Documents/workspace/mocks/shifu_src/test/resources/cert.pem')
+  },
 });
